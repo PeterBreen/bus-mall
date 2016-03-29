862530a9d4a942eda115782de6556f7ac46e07c2
Peter Breen <pbreen@gmail.com>
2016-03-29 11:01:25 -0700

    add some initial function handling for random selection

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 //GLOBAL VARIABLES!
+//total number of clicks - after 25 offer choice to see stats
 var totalClicks = 0;
+//initialize product array
 var productArray = [];
-//all product variables
+//initialize all product variables
 var bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulu, dogduck, dragon, pen, petsweep, scissors, shark, sweep, tauntaun, unicorn, usb, watercan, wineglass;
 
-//Object Constructor Probably
+//OBJECT CONSTRUCTORS
 //url should return the img link for display on page, displayCount is number of times this shows on the page, clickCount is the number of times this image was clicked on, and Display Name is the friendly name for SWEET CHARTS
 function ProductSelection(imageLink, displayName){
   this.imageLink = imageLink;
@@ -12,6 +14,22 @@ function ProductSelection(imageLink, displayName){
   this.clickCount = 0;
   this.displayName = displayName;
 }
+
+//FUNCTION PARTY TOWNHOUSE, BYOB
+function selectNewImages() {
+  for (i = 0; i < 3; i++) {
+    //pick image from the productArray
+    var randNum = getRandomIntInclusive(0, productArray.length);
+    
+    //create element for DOM
+    //attach element to DOM
+    //iterate ++ on displayCount
+  }
+}
+
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
 //create objects - one per product, then push into productArray;
 bag = productArray.push(new ProductSelection('img/bag.jpg','R2D2 Luggage'));
@@ -34,6 +52,8 @@ unicorn = productArray.push(new ProductSelection('img/unicorn.jpg','Unicorn Meat
 usb = productArray.push(new ProductSelection('img/usb.gif','USB Powered Tentacle'));
 watercan = productArray.push(new ProductSelection('img/water-can.jpg','Boring Watering Can'));
 wineglass = productArray.push(new ProductSelection('img/wine-glass.jpg','Goofy Wineglass'));
+
+
 
 //event handler - totalClicks++ counts total number of image iterations so the option for charts can be set at 25
 function handleImageClick(event){
