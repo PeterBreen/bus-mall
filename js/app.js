de07c4029d62ae14c625ff0e7fadaba231ae0ee3
Peter Breen <pbreen@gmail.com>
2016-03-30 10:46:31 -0700

    functioning chart labels and data WHAAAAAAAAAT

--- js/app.js
+++ js/app.js
@@ -53,24 +53,36 @@ function clearImages() {
 function displayCharts() {
   console.log('LOOKS LIKE IT IS CHART TIME');
   //SPECIAL CHART SUB-ZONE #4: I HATE UNDERWATER LEVELS IN MARIO GAMES
+  var productLabelsArray = [];
+  for (i = 0; i < productArray.length; i++) {
+    productLabelsArray.push(productArray[i].displayName);
+  }
+  var productClicksArray = [];
+  for (i = 0; i < productArray.length; i++) {
+    productClicksArray.push(productArray[i].clickCount);
+  }
+  var productDisplayArray = [];
+  for (i = 0; i < productArray.length; i++) {
+    productDisplayArray.push(productArray[i].displayCount);
+  }
   var dataResults = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: productLabelsArray,
     datasets: [
       {
-        label: 'My First dataset',
+        label: 'Times Displayed',
         fillColor: 'rgba(220,220,220,0.5)',
         strokeColor: 'rgba(220,220,220,0.8)',
         highlightFill: 'rgba(220,220,220,0.75)',
         highlightStroke: 'rgba(220,220,220,1)',
-        data: [65, 59, 80, 81, 56, 55, 40]
+        data: productDisplayArray
       },
       {
-        label: 'My Second dataset',
+        label: 'Times Clicked',
         fillColor: 'rgba(151,187,205,0.5)',
         strokeColor: 'rgba(151,187,205,0.8)',
         highlightFill: 'rgba(151,187,205,0.75)',
         highlightStroke: 'rgba(151,187,205,1)',
-        data: [28, 48, 40, 19, 86, 27, 90]
+        data: productClicksArray
       }
     ]
   };
